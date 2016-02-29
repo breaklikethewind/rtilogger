80295ea511924c7d4dcd6479b65fcc00a124d4c6
eric <breaklikethewind@mchsi.com>
2016-02-29 14:31:33 -0600

    Be less aggresive with queue locks

--- rtilogger.c
+++ rtilogger.c
@@ -164,17 +164,18 @@ void write_queue_to_file(void)
 	struct queue_entry_t* queue_stale;
 
 	// Fetch file queue data
-	pthread_mutex_lock(&lock);		
 	while (queue_head)
 	{
+		pthread_mutex_lock(&lock);
 		write(status.file_txt_desc, queue_head->buf, strlen(queue_head->buf));
 		queue_stale = queue_head;
 		queue_head = queue_head->next;
 		free(queue_stale);
 		if (queue_head == NULL)
 			queue_tail = NULL;
+		pthread_mutex_unlock(&lock);	
+		sleep(0);
 	}
-	pthread_mutex_unlock(&lock);
 }
 
 void *thread_write_file( void *ptr ) 
